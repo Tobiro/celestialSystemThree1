b048e00e72df93e3222b6fe8ce3b1aed14d69118
Tobiro <shibahsish.behera@gmail.com>
2024-01-04 15:51:27 +0530

    with trjeactories. need to cleanup traj code + n body simulations

--- rocket.js
+++ rocket.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'stats.js'
+import {matrix} from 'mathjs'
+import {zeros} from 'mathjs'
+import {sum} from 'mathjs'
 
 
 const stats = new Stats();
@@ -18,7 +21,7 @@ var stretch = 100;
 const g = 9.8;
 const c = 299792458;
 const G = 6.6743 * Math.pow(10,-11);
-var delT = 1;
+var delT = 2;
 const AU = 1.495978707* Math.pow(10,11);
 var distanceToScreenMult = stretch/AU;  
 
@@ -90,31 +93,26 @@ class Rocket {
             this.rocketG.setAttribute('position', new THREE.BufferAttribute(this.vertices,3)); //NOTE: Maybe change the verices to 3X2 from 3X3 and take 2 at a time from the method
             this.rokcetM = new THREE.MeshPhongMaterial({color:0x000fff});
             this.rokcetM.side = THREE.DoubleSide;
-            this.rocket = new THREE.Mesh(this.rocketG, this.rokcetM);
-            this.rocket.position.copy(new THREE.Vector3(X,Y,0));
+            this.body = new THREE.Mesh(this.rocketG, this.rokcetM);
+            this.body.position.copy(new THREE.Vector3(X,Y,0));
     }
     movePos(dd, dt){
-        this.rocket.position.add(dd.multiplyScalar(dt));
+        this.body.position.add(dd.clone().multiplyScalar(dt));
+        //console.log(dd)
     }
     moveVel(dv,dt){
-        this.V.add(dv.multiplyScalar(delT));
+        this.V.add(dv.clone().multiplyScalar(dt));
+        //console.log(dv)
     }
+    /* NOTE: This function is not being used after phyeng was written*/
     forceOnRocket(body){
         let r = new THREE.Vector3();
-        r.subVectors(body.planet.position,this.rocket.position )
+        r.subVectors(body.body.position,this.body.position )
         let rnorm = r.clone().normalize()
         //let acc = rnorm.multiplyScalar(1.993* Math.pow(10,(-38))*body.mass/r.distanceToSquared(new THREE.Vector3()) )
         let acc = rnorm.multiplyScalar(G*Math.pow(speedToScreen(1),3)*body.mass/r.distanceToSquared(new THREE.Vector3()) )
         return acc;
     };
-    _trajForce(body,pos){
-        let r = new THREE.Vector3();
-        r.subVectors(body.planet.position,pos )
-        let rnorm = r.clone().normalize()
-        //let acc = rnorm.multiplyScalar(1.993* Math.pow(10,(-38))*body.mass/r.distanceToSquared(new THREE.Vector3()) )
-        let acc = rnorm.multiplyScalar(G*Math.pow(speedToScreen(1),3)*body.mass/r.distanceToSquared(new THREE.Vector3()) )
-        return acc;
-    }
     thrusters(left,right,up,down){
         let acc1 = new THREE.Vector3();
         let acc2 = new THREE.Vector3();
@@ -142,15 +140,35 @@ class Rocket {
         acc1.add(acc5);
         return acc1;
     };
+    _trajForce(body,pos){
+        let r = new THREE.Vector3();
+        r.subVectors(body.body.position,pos )
+        let rnorm = r.clone().normalize()
+        //let acc = rnorm.multiplyScalar(1.993* Math.pow(10,(-38))*body.mass/r.distanceToSquared(new THREE.Vector3()) )
+        let acc = rnorm.multiplyScalar(G*Math.pow(speedToScreen(1),3)*body.mass/r.lengthSq() )
+        return acc;
+    }
+    _trajForceNew(bodies, pos){
+        let siz = bodies.length
+        let r = Array.from({ length: siz }, (x, i) => new THREE.Vector3);
+        let rnorm = Array.from({ length: siz }, (x, i) => new THREE.Vector3);
+        let acc = new THREE.Vector3();
+        for(let i=0;i<siz;i++){
+            r[i].subVectors(bodies[i].body.position, pos);
+            rnorm[i] = r[i].clone().normalize()
+            acc.add(rnorm[i].multiplyScalar(G*Math.pow(speedToScreen(1),3) * bodies[i].mass/r[i].lengthSq() )) 
+        }
+        return acc
+    }
     trajectory(dt){
         let trajPtsCnt = 300;
-        let trajPtsPos = [this.rocket.position.clone()];
+        let trajPtsPos = [this.body.position.clone()];
         let trajPtsV = [this.V.clone()];
         for(let i=0; i<trajPtsCnt;i++){
             let trajPtsPosTemp = trajPtsPos[i].clone();
             let trajPtsVTemp = trajPtsV[i].clone();
             for(let j=0;j<1000;j++){
-                let acc = this._trajForce(p1,trajPtsPosTemp);
+                let acc = this._trajForceNew(pBodies,trajPtsPosTemp);
                 trajPtsVTemp.add(acc.multiplyScalar(dt*20));
                 trajPtsPosTemp.add(trajPtsVTemp.clone().multiplyScalar(dt*20));
             }
@@ -158,7 +176,7 @@ class Rocket {
             trajPtsPos.push(trajPtsPosTemp)
         }
         let trajG = new THREE.BufferGeometry().setFromPoints(trajPtsPos);
-        let trajM = new THREE.LineDashedMaterial( { color: 0x000fff, dashSize: 0.5, gapSize: 2 } )
+        let trajM = new THREE.LineDashedMaterial( { color: 0x000fff, dashSize: 3, gapSize: 300 } )
         let traj = new THREE.Line(trajG, trajM) ;
         return traj;
     }
@@ -175,23 +193,87 @@ class Planet {
         this.mass = mass
         this.planetG = new THREE.CircleGeometry(this.radius, this.segments);
         this.planetM = new THREE.MeshPhongMaterial({color:0x000fff});
-        this.planet = new THREE.Mesh(this.planetG, this.planetM);
-        this.planet.position.copy(new THREE.Vector3(X,Y,0))
+        this.body = new THREE.Mesh(this.planetG, this.planetM);
+        this.body.position.copy(new THREE.Vector3(X,Y,0))
     }
     movePos(dd,dt){
-        this.planet.position.add(dd.multiplyScalar(dt));
+        this.body.position.add(dd.clone().multiplyScalar(dt));
     }
     moveVel(dv, dt){
-        this.V.add(dv.multiplyScalar(dt));
+        this.V.add(dv.clone().multiplyScalar(dt));
     }
 }
 
+/**PHYSICS ENGINE******************************/
+/*********************************************** */
+
+function forceCalc(body2, r){
+    // let r = new THREE.Vector3();
+    // r.subVectors(body.body.position,this.body.position )
+    //console.log(r.length())
+    if(r.length() != 0){
+        let rnorm = r.clone().normalize()
+        let acc = rnorm.multiplyScalar(G*Math.pow(speedToScreen(1),3)*body2.mass/r.distanceToSquared(new THREE.Vector3()) )
+        return acc;
+    } else {
+        return new THREE.Vector3();
+    }
+    
+};
+
+//let aa = matrix([[1,2], [3,4]])
+//sum(aa)
+//console.log(sum(aa,0).get([1]))
+//console.log(aa.size()[1])
+
+
+function matVecAdd(mat){
+    let siz = mat.size()[0];
+    let output = Array.from({ length: siz }, (x, i) => new THREE.Vector3);
+    for(let i=0;i<siz;i++){
+        for(let j=0;j<siz;j++){
+            output[i].add(mat.get([parseInt(i),parseInt(j)]))
+        }
+    }
+    //console.log(output)
+    return output
+}
+
+/* NOTE: calling mathjs for these simple mat calc is unnecesary. Change this later */
+function phyEng(bodies){
+    let matPos = zeros(bodies.length, bodies.length)
+    let matAcc = zeros(bodies.length, bodies.length)
+    for(let i in bodies){
+        for(let j in bodies){
+            matPos.set([parseInt(i),parseInt(j)], bodies[j].body.position.clone().sub(bodies[i].body.position))
+            matAcc.set([parseInt(i),parseInt(j)], forceCalc(bodies[j], matPos.get([parseInt(i),parseInt(j)])))
+
+            //console.log(matAcc.get([parseInt(i),parseInt(j)]))
+            //console.log(i,j)
+        }
+    }
+    return matVecAdd(matAcc);
+    //return sum(matAcc,1)
+    // let aa = sum(matAcc,1)
+    // console.log(aa.get([0]))
+}
+
+
+
+
+/*************************OBJECT INSTATIATION + SCENE*********************** */
+/******************************************************************************** */
 
 const r1 = new Rocket(-distToScreen(AU),0,0.0000,speedToScreen(/*0.0002113*/ 0.000105   *c) ,1,1.5,1,1 );
 //const r1 = new Rocket(0,0,0.0000,Math.sqrt(9*Math.pow(10,30 ) *1.993* Math.pow(10,(-38))/30 ) ,1,1.5,1,1 );
 const p1 = new Planet(0,0,0,0,2, 50, 1,9*Math.pow(10,30));
+const p2 = new Planet(100,50,0,0,2, 50, 1,9*Math.pow(10,30));
 
 //console.log(r1.forceOnRocket(p1).x * AU/100);
+
+ var bodies = [r1,p1,p2]
+// console.log(phyEng(bodies))
+var pBodies = [p1,p2]
 
 
 const scene = new THREE.Scene();
@@ -208,8 +290,9 @@ const helper = new THREE.DirectionalLightHelper(dl,1);
 dl.translateY(10)
 //console.log(dl.position)
 //helper.translateX(-20);
-scene.add(r1.rocket);
-scene.add(p1.planet);
+scene.add(r1.body);
+scene.add(p1.body);
+scene.add(p2.body);
 scene.add(lght);
 scene.add(dl);
 scene.add(helper);
@@ -217,6 +300,11 @@ scene.add(line);
 //scene.add(r1.trajectory(delT))
 camera.position.z = 100;
 //console.log(r1.trajectory(delT*100))
+
+
+
+/**********************ANIMATE LOOP********************************************** */
+/******************************************************************************** */
 var traj_to_ren_rem;
 var trajCnt = 0;
 var toggler = 0;
@@ -228,33 +316,24 @@ function animate(now) {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 	controls.update();
-   let cntr = 9000
+   let cntr = 3000
 
     for(let i = 0;i<cntr; i++){
-        let acc1 = r1.forceOnRocket(p1);
+        //let acc1 = r1.forceOnRocket(p1);
+        let acc1 = phyEng(bodies);
+        //console.log(delT)
         let acc2 = r1.thrusters(left,right,up,down);
-        r1.moveVel(acc1, delT);
-        r1.moveVel(acc2.multiplyScalar(delT*10));
+        //console.log(acc2);
+        r1.moveVel(acc1[0], delT);
+        r1.moveVel(acc2,(delT*10));
         // if(right === 1){
         //     let acc2 = r1.thrusters(0,1,0,0);
         //     r1.moveVel(acc2);
         // }
         r1.movePos(r1.V, delT);
-        dl.position.x = r1.rocket.position.x
-        // if(acc2.length()>0){
-        //     if(trajCnt === 0){
-        //         traj_to_ren_rem = r1.trajectory(delT);
-        //         scene.add(traj_to_ren_rem);
-        //         trajCnt++
-        //     };
-        //     break;
-        // }
-        // else {
-        //     if(trajCnt >0){
-        //         scene.remove(traj_to_ren_rem);
-        //         trajCnt = 0
-        //     }
-        // }
+        dl.position.x = r1.body.position.x
+        dl.position.y = r1.body.position.y+3
+
         if(acc2.length()>0){
             if(trajCnt === 0){
                 trajCnt++
@@ -274,39 +353,26 @@ function animate(now) {
                     }
                     }
                 trajCnt++;
-                
-                // traj_to_ren_rem = r1.trajectory(delT);
-                // scene.add(traj_to_ren_rem);
-
-                 //scene.remove(traj_to_ren_rem);   
             }
             break;
         }
         else {
-            //scene.remove(traj_to_ren_rem)
-            //trajCnt++
-            //console.log(trajCnt)
             if(trajCnt >0){
                 scene.remove(traj_to_ren_rem);
                 traj_to_ren_rem = r1.trajectory(delT);
                 scene.add(traj_to_ren_rem);
-                console.log('this')
                 trajCnt = 0
             }
-            //scene.remove(traj_to_ren_rem)
         }
     }
-    //scene.remove(r1.rocke)
-    //let xxx = r1.thrusters(left,right,up,down)
-    //console.log(r1.thrusters(left, right, up,down))
-    //console.log(r1.V)
-    //console.log(acc)
-    //console.log(left,right,up,down);
-    //console.log(r1.V.y * AU/100)
-    //console.log(acc1);
    stats.end();
 }
 animate();
+
+
+/**************************ARROW KEY ASYNC********************************/
+/******************************************************************************** */
+
 
 $(document).ready(()=>{
 	$(document).keydown(
